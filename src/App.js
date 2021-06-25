ebce98d3cc9a884f5d71f0836ef5d1e2bfc651da
cboscariol <caamila.boscariol@gmail.com>
2021-06-25 16:13:39 -0300

    like-system

--- src/App.js
+++ src/App.js
@@ -2,19 +2,43 @@ import './App.css';
 import { useState } from "react";
 import black from './assets/black.svg'
 import yellow from './assets/yellow.svg'
+import liked from "./assets/like-svgrepo-com.svg";
+import dislike from "./assets/dislike-svgrepo-com.svg"
+import blackHeart from "./assets/blackHeart.svg"
+import redHeart from "./assets/red-heart.svg"
 
 
 function App() {
-  const [liked, setLiked] = useState(false)
+  const [star, setStar] = useState(false)
+  const [like, setLike] = useState(false)
+  const [heart, setHeart] = useState(false)
   const [number, setNumber] = useState()
 
-  function isLiked() {
-    setLiked(!liked);
+  function isStar() {
+    setStar(!star);
     return;
   }
 
   function starImage() {
-    return liked ? yellow : black
+    return star ? yellow : black
+  }
+
+  function isHearted() {
+    setHeart(!heart);
+    return;
+  }
+
+  function heartImage() {
+    return heart ? blackHeart : redHeart
+  }
+
+  function isLike() {
+    setLike(!like);
+    return;
+  }
+
+  function likeImage() {
+    return like ? liked : dislike
   }
 
   function numberState() {
@@ -28,8 +52,11 @@ function App() {
 
   return (
     <div className="App">
-      <img src={starImage()} alt="" onClick={isLiked} className="imageSize" />
-      <button type="button" class="btn btn-primary" onClick={numberState}>{changeNumber()}</button>
+      <img src={starImage()} alt="" onClick={isStar} className="imageSize" />
+      <button type="button" className="btn btn-primary" onClick={numberState}>{changeNumber()}</button>
+      <button type="button" className="btn btn-light imageSize" onClick={isLike}><img src={likeImage()} alt="" /></button>
+      <button type="button" className="btn btn-light imageSize" onClick={isHearted}><img src={heartImage()} alt="" /></button>
+
     </div>
   );
 }
